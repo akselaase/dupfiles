5fe83811c143e31d723441a21caba9b7c902f14d
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2020-07-02 20:38:41 +0200

    project start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DupFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
